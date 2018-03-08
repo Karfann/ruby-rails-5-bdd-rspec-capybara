2631bbc271ebbe744eda8171e51bb1d6c65bdac0
Karfann Nacif <karfannacif@gmail.com>
2018-03-08 10:46:12 +0000

    Implemented listing articles

---
+++ spec/features/listing_article_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.feature "Listing Articles" do
+
+    before do
+        @article1 = Article.create!(title: 'My first article', body: 'Lorem ipsum dolor sit')
+        @article2 = Article.create!(title: 'My second article', body: 'Body of 2nd article')
+    end
+
+    scenario "A user list all articles" do
+        visit "/"
+
+        expect(page).to have_content(@article1.title)
+        expect(page).to have_content(@article1.body)
+
+        expect(page).to have_content(@article2.title)
+        expect(page).to have_content(@article2.body)
+
+        expect(page).to have_link(@article1.title)
+        expect(page).to have_link(@article2.title)
+    end
+end
